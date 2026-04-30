9b02efcdc0ff6f7714acb4873c32667f90b62f62
TomPlop <tomdidome@gmail.com>
2026-04-30 07:55:10 -0400

    Brazier gives back tfc salt rather that hexalia:salt

---
+++ src/main/java/com/terrafirmamagica/mixins/common/hexalia/RitualBrazierBlockMixin.java
@@ -0,0 +1,26 @@
+package com.terrafirmamagica.mixins.common.hexalia;
+
+import net.dries007.tfc.common.items.TFCItems;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.item.ItemStack;
+import net.astralya.hexalia.block.custom.RitualBrazierBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static net.dries007.tfc.common.items.Powder.SALT;
+
+@Mixin(value = RitualBrazierBlock.class, remap = false)
+public abstract class RitualBrazierBlockMixin {
+
+    @Redirect(
+            method = "onRemove",
+            at = @At(
+                    value = "NEW",
+                    target = "(Lnet/minecraft/world/level/ItemLike;)Lnet/minecraft/world/item/ItemStack;"
+            )
+    )
+    private ItemStack redirectSaltDrop(ItemLike item) {
+        return new ItemStack(TFCItems.POWDERS.get(SALT).get());
+    }
+}
--- src/main/java/com/terrafirmamagica/mixins/common/hexalia/SmallCauldronBlockMixin.java
+++ src/main/java/com/terrafirmamagica/mixins/common/hexalia/SmallCauldronBlockMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import net.dries007.tfc.common.items.TFCItems;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
